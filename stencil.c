4d0dfbda676e4defa287a13eab9e4a52f52e1e74
Louis Heath <lheath1998@gmail.com>
2018-10-15 19:43:33 +0100

    removed division completely, but times are slower..

--- stencil.c
+++ stencil.c
@@ -52,33 +52,33 @@ int main(int argc, char *argv[]) {
 void stencil(const int nx, const int ny, double *  image, double *  tmp_image) {
   // i = 0
   for (int j = 1; j < ny-1; ++j) {
-    tmp_image[j] =  image[j] * 0.6 + image[j+ny] * 0.1 + image[j-1] * 0.1 + image[j+1] * 0.1;
+    tmp_image[j] =  image[j] * 6 + image[j+ny] + image[j-1] + image[j+1];
   }
   // i = (nx-1)
   for (int j = 1; j < ny-1; ++j) {
-    tmp_image[j+(nx-1)*ny] = image[j+(nx-1)*ny] * 0.6 + image[j+(nx-2)*ny] * 0.1 + image[j-1+(nx-1)*ny] * 0.1 + image[j+1+(nx-1)*ny] * 0.1;
+    tmp_image[j+(nx-1)*ny] = image[j+(nx-1)*ny] * 6 + image[j+(nx-2)*ny] + image[j-1+(nx-1)*ny] + image[j+1+(nx-1)*ny];
   }
   // j = 0
   for (int i = 1; i < nx-1; ++i) {
-    tmp_image[i*ny] = image[i*ny] * 0.6 + image[(i-1)*ny] * 0.1 + image[(i+1)*ny] * 0.1 + image[1+i*ny] * 0.1;
+    tmp_image[i*ny] = image[i*ny] * 6 + image[(i-1)*ny] + image[(i+1)*ny] + image[1+i*ny];
   }
   // j = (ny-1)
   for (int i = 1; i < nx-1; ++i) {
-    tmp_image[ny-1+i*ny] = image[ny-1+i*ny] * 0.6 + image[ny-1+(i-1)*ny] * 0.1 + image[ny-1+(i+1)*ny] * 0.1 + image[ny-2+i*ny] * 0.1;
+    tmp_image[ny-1+i*ny] = image[ny-1+i*ny] * 6 + image[ny-1+(i-1)*ny] + image[ny-1+(i+1)*ny] + image[ny-2+i*ny];
   }
   // i = 0, j = 0
-  tmp_image[0] = image[0] * 0.6 + image[ny] * 0.1 + image[1] * 0.1;
+  tmp_image[0] = image[0] * 6 + image[ny] + image[1];
   // i = 0; j = ny-1
-  tmp_image[ny-1] = image[ny-1] * 0.6 + image[2*ny-1] * 0.1 + image[ny-2] * 0.1;
+  tmp_image[ny-1] = image[ny-1] * 6 + image[2*ny-1] + image[ny-2];
   // i = (nx-1), j = ny-1
-  tmp_image[1+nx*ny] =  image[1+nx*ny] * 0.6 + image[1+(nx-1)*ny] * 0.1 + image[2+nx*ny] * 0.1;
+  tmp_image[1+nx*ny] =  image[1+nx*ny] * 6 + image[1+(nx-1)*ny] + image[2+nx*ny];
   // i = (nx-1), j = 0
-  tmp_image[(nx-1)*ny] = image[(nx-1)*ny] * 0.6 + image[(nx-2)*ny] * 0.1 + image[1+(nx-1)*ny] * 0.1;
+  tmp_image[(nx-1)*ny] = image[(nx-1)*ny] * 6 + image[(nx-2)*ny] + image[1+(nx-1)*ny];
 
   // all non-edge squares
   for (int j = 1; j < ny-1; ++j) {
     for (int i = 1; i < nx-1; ++i) {
-      tmp_image[j+i*ny] = image[j+i*ny] * 0.6 + image[j+(i-1)*ny] * 0.1 + image[j+(i+1)*ny] * 0.1 + image[j-1+i*ny] * 0.1 + image[j+1+i*ny] * 0.1;
+      tmp_image[j+i*ny] = image[j+i*ny] * 6 + image[j+(i-1)*ny] + image[j+(i+1)*ny] + image[j-1+i*ny] + image[j+1+i*ny];
     }
   }
 }
