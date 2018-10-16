50943c56e95315def02d6d18c0f36560e41993c1
Louis Heath <lheath1998@gmail.com>
2018-10-16 15:12:34 +0100

    separating edge loops allows for one of them to vectorise

--- stencil.c
+++ stencil.c
@@ -55,12 +55,17 @@ void stencil(const int nx, const int ny, double * restrict image, double * restr
   for (int j = 1; j < ny-1; ++j) {
     // i = 0
     tmp_image[j] =  image[j] * 6 + image[j+ny] + image[j-1] + image[j+1];
+  }
+  // non-corner edge cells
+  for (int j = 1; j < ny-1; ++j) {
     // i = (nx-1)
     tmp_image[j+(nx-1)*ny] = image[j+(nx-1)*ny] * 6 + image[j+(nx-2)*ny] + image[j-1+(nx-1)*ny] + image[j+1+(nx-1)*ny];
   }
   for (int i = 1; i < nx-1; ++i) {
     // j = 0
     tmp_image[i*ny] = image[i*ny] * 6 + image[(i-1)*ny] + image[(i+1)*ny] + image[1+i*ny];
+  }
+  for (int i = 1; i < nx-1; ++i) {
     // j = (ny-1)
     tmp_image[ny-1+i*ny] = image[ny-1+i*ny] * 6 + image[ny-1+(i-1)*ny] + image[ny-1+(i+1)*ny] + image[ny-2+i*ny];
   }
