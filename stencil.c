839bc0715c35c19bc280c771a32a0091d925a06d
Louis Heath <lheath1998@gmail.com>
2018-10-18 12:00:22 +0100

    reordered loops to go left to right, top down

--- stencil.c
+++ stencil.c
@@ -51,38 +51,43 @@ int main(int argc, char *argv[]) {
 
 void stencil(const int nx, const int ny, double * restrict image, double * restrict tmp_image) {
 
-  // non-corner edge cells
-  for (int j = 1; j != ny-1; ++j) {  // VECTORIZING
-    // i = (nx-1)
-    tmp_image[j+(nx-1)*ny] = image[j+(nx-1)*ny] * 6 + image[j+(nx-2)*ny] + image[j-1+(nx-1)*ny] + image[j+1+(nx-1)*ny];
-  }
+  // i = 0, j = 0
+  tmp_image[0] = image[0] * 6 + image[ny] + image[1];
+
+  // 'top' horizontal edge cells
   for (int j = 1; j != ny-1; ++j) {  // VECTORIZING
     // i = 0
     tmp_image[j] =  image[j] * 6 + image[j+ny] + image[j-1] + image[j+1];
   }
 
-  // corner cells
-  // i = 0, j = 0
-  tmp_image[0] = image[0] * 6 + image[ny] + image[1];
   // i = 0; j = ny-1
   tmp_image[ny-1] = image[ny-1] * 6 + image[2*ny-1] + image[ny-2];
-  // i = (nx-1), j = ny-1
-  tmp_image[1+nx*ny] =  image[1+nx*ny] * 6 + image[1+(nx-1)*ny] + image[2+nx*ny];
-  // i = (nx-1), j = 0
-  tmp_image[(nx-1)*ny] = image[(nx-1)*ny] * 6 + image[(nx-2)*ny] + image[1+(nx-1)*ny];
 
   // non-edge cells
   for (int i = 1; i != nx-1; ++i) {
     // j = 0
     tmp_image[i*ny] = image[i*ny] * 6 + image[(i-1)*ny] + image[(i+1)*ny] + image[1+i*ny];
 
-    for (int j = 1; j != ny-1; ++j) { 
+    for (int j = 1; j != ny-1; ++j) {  // VECTORIZING
       tmp_image[j+i*ny] = image[j+i*ny] * 6 + image[j+i*ny-ny] + image[j+i*ny+ny] + image[j+i*ny-1] + image[j+i*ny+1];
     }
 
     // j = (ny-1)
     tmp_image[ny-1+i*ny] = image[ny-1+i*ny] * 6 + image[ny-1+(i-1)*ny] + image[ny-1+(i+1)*ny] + image[ny-2+i*ny];
   }
+
+  // i = (nx-1), j = 0
+  tmp_image[(nx-1)*ny] = image[(nx-1)*ny] * 6 + image[(nx-2)*ny] + image[1+(nx-1)*ny];
+
+  // 'bottom' horizontal edge cells
+  for (int j = 1; j != ny-1; ++j) {  // VECTORIZING
+    // i = (nx-1)
+    tmp_image[j+(nx-1)*ny] = image[j+(nx-1)*ny] * 6 + image[j+(nx-2)*ny] + image[j-1+(nx-1)*ny] + image[j+1+(nx-1)*ny];
+  }
+
+  // i = (nx-1), j = ny-1
+  tmp_image[1+nx*ny] =  image[1+nx*ny] * 6 + image[1+(nx-1)*ny] + image[2+nx*ny];
+  
 }
 
 // Create the input image
