50a6d0dfb0c3562ac1a5de7dfc52c9031f854043
Louis Heath <lheath1998@gmail.com>
2018-10-18 12:17:22 +0100

    tried working with floats instead

--- stencil.c
+++ stencil.c
@@ -6,9 +6,9 @@
 // Define output file name
 #define OUTPUT_FILE "stencil.pgm"
 
-void stencil(const int nx, const int ny, double *  image, double *  tmp_image);
-void init_image(const int nx, const int ny, double *  image, double *  tmp_image);
-void output_image(const char * file_name, const int nx, const int ny, double *image);
+void stencil(const int nx, const int ny, float *  image, float *  tmp_image);
+void init_image(const int nx, const int ny, float *  image, float *  tmp_image);
+void output_image(const char * file_name, const int nx, const int ny, float *image);
 double wtime(void);
 
 int main(int argc, char *argv[]) {
@@ -25,8 +25,8 @@ int main(int argc, char *argv[]) {
   int niters = atoi(argv[3]);
 
   // Allocate the image
-  double *image = malloc(sizeof(double)*nx*ny);
-  double *tmp_image = malloc(sizeof(double)*nx*ny);
+  float *image = malloc(sizeof(double)*nx*ny);
+  float *tmp_image = malloc(sizeof(double)*nx*ny);
 
   // Set the input image
   init_image(nx, ny, image, tmp_image);
@@ -49,49 +49,49 @@ int main(int argc, char *argv[]) {
   free(image);
 }
 
-void stencil(const int nx, const int ny, double * restrict image, double * restrict tmp_image) {
+void stencil(const int nx, const int ny, float * restrict image, float * restrict tmp_image) {
 
   // i = 0, j = 0
-  tmp_image[0] = image[0] * 6 + image[ny] + image[1];
+  tmp_image[0] = image[0] * 0.6f + image[ny] * 0.1f + image[1] * 0.1f;
 
   // 'top' horizontal edge cells
   for (int j = 1; j != ny-1; ++j) {  // VECTORIZING
     // i = 0
-    tmp_image[j] =  image[j] * 6 + image[j+ny] + image[j-1] + image[j+1];
+    tmp_image[j] =  image[j] * 0.6f + image[j+ny] * 0.1f + image[j-1] * 0.1f + image[j+1] * 0.1f;
   }
 
   // i = 0; j = ny-1
-  tmp_image[ny-1] = image[ny-1] * 6 + image[2*ny-1] + image[ny-2];
+  tmp_image[ny-1] = image[ny-1] * 0.6f + image[2*ny-1] * 0.1f + image[ny-2] * 0.1f;
 
   // non-edge cells
   for (int i = 1; i != nx-1; ++i) {
     // j = 0
-    tmp_image[i*ny] = image[i*ny] * 6 + image[(i-1)*ny] + image[(i+1)*ny] + image[1+i*ny];
+    tmp_image[i*ny] = image[i*ny] * 0.6f + image[(i-1)*ny] * 0.1f + image[(i+1)*ny] * 0.1f + image[1+i*ny] * 0.1f;
 
     for (int j = 1; j != ny-1; ++j) {  // VECTORIZING
-      tmp_image[j+i*ny] = image[j+i*ny] * 6 + image[j+i*ny-ny] + image[j+i*ny+ny] + image[j+i*ny-1] + image[j+i*ny+1];
+      tmp_image[j+i*ny] = image[j+i*ny] * 0.6f + image[j+i*ny-ny] * 0.1f + image[j+i*ny+ny] * 0.1f + image[j+i*ny-1] * 0.1f + image[j+i*ny+1] * 0.1f;
     }
 
     // j = (ny-1)
-    tmp_image[ny-1+i*ny] = image[ny-1+i*ny] * 6 + image[ny-1+(i-1)*ny] + image[ny-1+(i+1)*ny] + image[ny-2+i*ny];
+    tmp_image[ny-1+i*ny] = image[ny-1+i*ny] * 0.6f + image[ny-1+(i-1)*ny] * 0.1f + image[ny-1+(i+1)*ny] * 0.1f + image[ny-2+i*ny] * 0.1f;
   }
 
   // i = (nx-1), j = 0
-  tmp_image[(nx-1)*ny] = image[(nx-1)*ny] * 6 + image[(nx-2)*ny] + image[1+(nx-1)*ny];
+  tmp_image[(nx-1)*ny] = image[(nx-1)*ny] * 0.6f + image[(nx-2)*ny] * 0.1f + image[1+(nx-1)*ny] * 0.1f;
 
   // 'bottom' horizontal edge cells
   for (int j = 1; j != ny-1; ++j) {  // VECTORIZING
     // i = (nx-1)
-    tmp_image[j+(nx-1)*ny] = image[j+(nx-1)*ny] * 6 + image[j+(nx-2)*ny] + image[j-1+(nx-1)*ny] + image[j+1+(nx-1)*ny];
+    tmp_image[j+(nx-1)*ny] = image[j+(nx-1)*ny] * 0.6f + image[j+(nx-2)*ny] * 0.1f + image[j-1+(nx-1)*ny] * 0.1f + image[j+1+(nx-1)*ny] * 0.1f;
   }
 
   // i = (nx-1), j = ny-1
-  tmp_image[1+nx*ny] =  image[1+nx*ny] * 6 + image[1+(nx-1)*ny] + image[2+nx*ny];
+  tmp_image[1+nx*ny] =  image[1+nx*ny] * 0.6f + image[1+(nx-1)*ny] * 0.1f + image[2+nx*ny] * 0.1f;
   
 }
 
 // Create the input image
-void init_image(const int nx, const int ny, double *  image, double *  tmp_image) {
+void init_image(const int nx, const int ny, float *  image, float *  tmp_image) {
   // Zero everything
   for (int j = 0; j < ny; ++j) {
     for (int i = 0; i < nx; ++i) {
@@ -114,7 +114,7 @@ void init_image(const int nx, const int ny, double *  image, double *  tmp_image
 }
 
 // Routine to output the image in Netpbm grayscale binary image format
-void output_image(const char * file_name, const int nx, const int ny, double *image) {
+void output_image(const char * file_name, const int nx, const int ny, float *image) {
 
   // Open output file
   FILE *fp = fopen(file_name, "w");
